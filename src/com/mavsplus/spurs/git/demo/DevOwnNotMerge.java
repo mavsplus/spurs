7b215d93207a4e371602092dcac7f22271eed04f
landon <mavsplus@aliyun.com>
2017-05-12 15:01:43 +0800

    dev own

---
+++ src/com/mavsplus/spurs/git/demo/DevOwnNotMerge.java
@@ -0,0 +1,15 @@
+package com.mavsplus.spurs.git.demo;
+
+/**
+ * Dev分支独有文件,不合并到master
+ * 
+ * @createDate 2017-05-12
+ * @author landon
+ * @since 1.8
+ */
+public class DevOwnNotMerge {
+
+	public static void main(String... args) {
+		System.out.println("Dev own");
+	}
+}
