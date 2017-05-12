b2167bfe33ccf60d6fb4c90771d938a8eb210620
landon <mavsplus@aliyun.com>
2017-05-12 15:35:53 +0800

    dev own add

--- src/com/mavsplus/spurs/git/demo/DevOwnNotMerge.java
+++ src/com/mavsplus/spurs/git/demo/DevOwnNotMerge.java
@@ -12,5 +12,6 @@ public class DevOwnNotMerge {
 	public static void main(String... args) {
 		System.out.println("Dev own");
 		System.out.println("own modify");
+		System.out.println("own add");
 	}
 }
